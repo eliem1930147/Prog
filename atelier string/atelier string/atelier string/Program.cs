b91f4ba15eeb49c91b3f106ba4ba978ce7e1cb7b
eliem1930147 <54861258+eliem1930147@users.noreply.github.com>
2019-11-12 10:33:14 -0500

    Add files via upload

---
+++ atelier string/atelier string/atelier string/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atelier_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string maPhrase = "salut tout le monde";
+            int premierE = maPhrase.IndexOf("e");
+            string premierMot = maPhrase.Substring(0, premierE);
+            int premierEspace = maPhrase.IndexOf(" ");
+            string premierPhrase = maPhrase.Substring(0, premierEspace);
+            int dernierEspace = maPhrase.LastIndexOf(" ");
+            string dernierMot = maPhrase.Substring(dernierEspace + 1);
+            string phraseenmajuscule = maPhrase.ToUpper();
+            string phraseenminuscule = maPhrase.ToLower();
+
+            if (maPhrase.Contains("octopus")==true)
+            {
+                Console.WriteLine("Ma phrase contient octopus");
+            }
+            else
+            {
+                Console.WriteLine("Ma phrase ne contient pas octopus");
+            }
+            Console.WriteLine(premierE);
+            Console.WriteLine(premierPhrase);
+            Console.WriteLine(dernierMot);
+            Console.WriteLine(phraseenmajuscule);
+            Console.WriteLine(phraseenminuscule);
+
+                
+
+            Console.ReadKey();
+                
+        }
+    }
+}
